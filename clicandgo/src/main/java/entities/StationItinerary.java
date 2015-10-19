121ca46be343eabc44c8c65fe15db541df81bc28
Sabrine <Sabrine@172.16.220.145>
2015-10-19 22:46:09 +0200

    Changed order to position and stationitenarari to line
    Signed-off-by: Sabrine <Sabrine@172.16.220.145>

--- clicandgo/src/main/java/entities/StationItinerary.java
+++ clicandgo/src/main/java/entities/StationItinerary.java
@@ -5,7 +5,6 @@ import java.io.Serializable;
 import javax.persistence.CascadeType;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
@@ -14,36 +13,32 @@ import javax.persistence.Table;
  *
  */
 @Entity
-@Table(name="SI")
+@Table(name = "line")
 public class StationItinerary implements Serializable {
 
 	private StationItineraryId StationItineraryId;
-	private Integer order;
+	private Integer position;
 	private Double duration;
 	private static final long serialVersionUID = 1L;
 
-	
 	private Station station;
 	private Itinerary itinerary;
-	
+
 	public StationItinerary() {
 		super();
 	}
-	
-	
-
-
 
 	public StationItinerary(entities.StationItineraryId stationItineraryId,
-			Integer order, Double duration, Station station, Itinerary itinerary) {
+			Integer position, Double duration, Station station,
+			Itinerary itinerary) {
 		super();
-		this.StationItineraryId = new StationItineraryId(itinerary.getItineraryId(), station.getStationId());
-		this.order = order;
+		this.StationItineraryId = new StationItineraryId(
+				itinerary.getItineraryId(), station.getStationId());
+		this.position = position;
 		this.duration = duration;
 		this.station = station;
 		this.itinerary = itinerary;
 	}
-
 
 	@EmbeddedId
 	public StationItineraryId getStationItineraryId() {
@@ -54,12 +49,12 @@ public class StationItinerary implements Serializable {
 		StationItineraryId = stationItineraryId;
 	}
 
-	public Integer getOrder() {
-		return order;
+	public Integer getPosition() {
+		return position;
 	}
 
-	public void setOrder(Integer order) {
-		this.order = order;
+	public void setPosition(Integer position) {
+		this.position = position;
 	}
 
 	public Double getDuration() {
@@ -70,100 +65,27 @@ public class StationItinerary implements Serializable {
 		this.duration = duration;
 	}
 
-
 	@ManyToOne(cascade = CascadeType.MERGE)
-	//@JoinColumn(name = "idStation", referencedColumnName = "stationId", insertable = false, updatable = false)
+	// @JoinColumn(name = "idStation", referencedColumnName = "stationId",
+	// insertable = false, updatable = false)
 	public Station getStation() {
 		return station;
 	}
-
-
 
 	public void setStation(Station station) {
 		this.station = station;
 	}
 
-
 	@ManyToOne(cascade = CascadeType.MERGE)
-	//@JoinColumn(name = "idItinerary", referencedColumnName = "itineraryId", insertable = false, updatable = false)
+	// @JoinColumn(name = "idItinerary", referencedColumnName = "itineraryId",
+	// insertable = false, updatable = false)
 	public Itinerary getItinerary() {
 		return itinerary;
 	}
 
-
-
 	public void setItinerary(Itinerary itinerary) {
 		this.itinerary = itinerary;
 	}
-	
 
-	//
-	// private Station station;
-	// private Itinerary itinerary;
-	//
-	// public StationItinerary() {
-	// super();
-	// }
-	//
-	// public StationItinerary(entities.StationItineraryId stationItineraryId,
-	// Integer order,
-	// Double duration, Station station, Itinerary itinerary) {
-	// super();
-	// this.StationItineraryId = new
-	// StationItineraryId(itinerary.getItineraryId(),
-	// station.getStationId());
-	// this.order = order;
-	// this.duration = duration;
-	// this.station = station;
-	// this.itinerary = itinerary;
-	// }
-	//
-	// public Integer getOrder() {
-	// return this.order;
-	// }
-	//
-	// public void setOrder(Integer order) {
-	// this.order = order;
-	// }
-	//
-	// public Double getDuration() {
-	// return this.duration;
-	// }
-	//
-	// public void setDuration(Double duration) {
-	// this.duration = duration;
-	// }
-	//
-	// @EmbeddedId
-	// public StationItineraryId getStationItineraryId() {
-	// return StationItineraryId;
-	// }
-	//
-	// public void setStationItineraryId(StationItineraryId stationItineraryId)
-	// {
-	// StationItineraryId = stationItineraryId;
-	// }
-	//
-	// @ManyToOne
-	// @JoinColumn(name = "stationId", referencedColumnName = "stationId",
-	// insertable = false, updatable = false)
-	// public Station getStation() {
-	// return station;
-	// }
-	//
-	// public void setStation(Station station) {
-	// this.station = station;
-	// }
-	//
-	// @ManyToOne
-	// @JoinColumn(name = "itineraryId", referencedColumnName = "itineraryId",
-	// insertable = false, updatable = false)
-	// public Itinerary getItinerary() {
-	// return itinerary;
-	// }
-	//
-	// public void setItinerary(Itinerary itinerary) {
-	// this.itinerary = itinerary;
-	// }
-	//
+	
 }
