4e66ccb6dd8c94f071458268b8adc5ebdf519062
yosrgouider <=yosr.gouider@esprit.tn>
2015-11-04 11:59:21 +0100

    Signed-off-by: yosrgouider <=yosr.gouider@esprit.tn>

--- clicandgo-client/src/main/java/testsStation/TestDeleteStation.java
+++ clicandgo-client/src/main/java/testsStation/TestDeleteStation.java
@@ -1,16 +1,14 @@
 package testsStation;
 
-import entities.Station;
 import BusinessDelegator.StationDelegate;
+import entities.Station;
 
 public class TestDeleteStation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Station st= new Station();
-	   st = StationDelegate.doFindStationById(1);
-		System.out.print(StationDelegate.doDeleteStation(st));
+		Station station = new Station();
+		station = StationDelegate.doFindStationById(1);
+		System.out.print(StationDelegate.doDeleteStation(station));
 	}
-	
 
 }
